d00486f012a594389a0ebd948a26cb6443e579f4
Best think co <Best think co@192.168.1.107>
2020-04-26 02:27:19 +0300

    last

---
+++ src/main/java/corrona/session/app.java
@@ -0,0 +1,8 @@
+package corrona.session;
+
+public class app {
+	public String Sample() {
+		return "sample";
+	}
+
+}
---
+++ src/test/java/corrona/session/Apptest.java
@@ -0,0 +1,14 @@
+package corrona.session;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Apptest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
